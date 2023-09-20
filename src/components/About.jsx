47cae79a0b2525b605f857877afd94c104ab2364
GoncaloPinto03 <gongaspinto03@gmail.com>
2023-09-20 15:56:02 +0100

    final touches

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import person from '../assets/person.jpeg'
+import React from 'react';
 
 
 const About = () => {
@@ -8,11 +7,11 @@ const About = () => {
   return (
     <div className=' p-2 md:p-4 justify-center flex items-center'>
     <div name="about" className=' bg-[#4825c7]/80 md:p-8 p-4 rounded-3xl m-4 max-w-[1240px]' >
-        <div className='mx-auto grid md:grid-cols-2'>
-              <img className='flex mx-auto rounded-[10px]' src={person} alt='/'/>
+        <div className='mx-auto'>
+              
                 
             <div className='flex flex-col justify-center md:p-4 p-2 '>
-                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2 text-[#ffffff]'>ABOUT</h1>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2 text-[#f2f2f2]'>ABOUT</h1>
                 <p className='text-justify font-semibold text-[#f2f2f2]'>I'm a third-year student pursuing a Bachelor's degree in Computer Science and Engineering at the Faculty of Engineering, University of Porto. With a passion for programming and a strong desire to learn, I'm eager to embark on new professional experiences.
 
 As a student in the field of Computer Science, I engage in programming daily, continuously honing my skills. I thrive in team environments, having already contributed to group projects during my studies. I also enjoy working on personal projects, which drives me to explore new programming languages and expand my knowledge.
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,53 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import React from 'react';
 import skills from '../assets/skills.png';
 
 const Skills = () => {
 
-    const [slides, setSlides] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const imageFileNames = [];
-    const numberOfImages = 3; // Change this to the total number of images
-
-    for (let i = 1; i <= numberOfImages; i++) {
-      imageFileNames.push(`skill${i}.png`);
-    }
-
-    // Dynamically import all images and update the slides state
-    Promise.all(
-      imageFileNames.map((imageName) =>
-        import(`../assets/skills/${imageName}`).then((module) => ({
-          url: module.default,
-        }))
-      )
-    ).then((importedSlides) => setSlides(importedSlides));
-  }, []);
-
-  if (slides.length === 0) {
-    // Return a loading indicator or null while the images are being fetched
-    return null;
-  }
-
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
 
   return (
-    <div className='p-4 justify-center flex items-center'>
+    <div className='p-6 justify-center flex items-center'>
       <div name="skills" className='rounded-3xl w-[1240px] max-w-[1240px] '>
         <div className='flex flex-col justify-center p-2'>
           <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2 text-[#f2f2f2]'>SKILLS</h1>
