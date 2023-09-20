c924835d6088fca9b6c25490563f8539f45ea9e0
GoncaloPinto03 <gongaspinto03@gmail.com>
2023-09-20 16:36:22 +0100

    data.js update

--- src/utils/data.js
+++ src/utils/data.js
@@ -18,14 +18,14 @@ export const data = [
       name: 'FEUP Foods',
       image:
         'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3570&q=80',
-      text: 'Developed a visionary mobile application designed to empower users with informed choices regarding their weekly food consumption on the university campus. The app offers essential features such as weekly cafeteria menus (including meat, fish, vegetarian, diet, and other dishes), detailed nutritional information, user ratings, and comments to promote healthy eating habits on the FEUP campus. Technology/Frameworks: Flutter, Firebase.',
+      text: 'Developed a visionary mobile application designed to empower users with informed choices regarding their weekly food consumption on the university campus. The app offers essential features such as weekly cafeteria menus (including meat, fish, vegetarian, diet, and other dishes), detailed nutritional information, user ratings, and comments to promote healthy eating habits on the FEUP campus. Technology/Frameworks: Flutter, Firebase, Figma.',
     },
     {
       id: 4,
       name: 'Trouble Ticket System',
       image:
         'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3570&q=80',
-      text: 'Designed and developed a dynamic website aimed at optimizing and streamlining the process of managing trouble tickets. The website empowers users to efficiently submit, track, and resolve tickets while providing intuitive user interfaces and real-time reporting functionalities for monitoring ticket status and performance metrics. Technology/Frameworks: HTML, CSS, JavaScript, SQL, PHP.',
+      text: 'Designed and developed a dynamic website aimed at optimizing and streamlining the process of managing trouble tickets. The website empowers users to efficiently submit, track, and resolve tickets while providing intuitive user interfaces and real-time reporting functionalities for monitoring ticket status and performance metrics. Technology/Frameworks: HTML, CSS, JavaScript, SQL, PHP, Figma.',
     },
     {
       id: 5,
